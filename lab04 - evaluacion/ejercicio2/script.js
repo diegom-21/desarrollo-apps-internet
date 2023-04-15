fb3d006271661bc1d83520d6ec74b046e2e6f0a2
diegom-21 <jimmy.moreno@tecsup.edu.pe>
2023-04-15 12:28:28 -0500

    lab04

--- lab04 - evaluacion/ejercicio2/script.js
+++
@@ -1,9 +0,0 @@
-function validaredad() {
-
-    var edad = document.getElementById("edad").value;
-
-    if (edad < 18){
-        alert("Solo se admiten usuarios mayores de edad.");
-        return false;
-    }
-}
